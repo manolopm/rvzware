880ffb984f771d16893b720b5ac3ed1c901d9f7d
manolopm <manolopm@portatil-hp>
2009-04-27 23:14:34 +0100

    Comments in all files stuff

--- apps/Rvzware/controllers/SunController.h
+++ apps/Rvzware/controllers/SunController.h
@@ -1,15 +1,12 @@
 /*
- * CAPAWARE 3D Geographical Multilayer Framework Free Software
- * Copyright (C) 2009 Instituto Tecnológico de Canarias (ITC) and Universidad de Las Palmas de Gran Canaria (ULPGC)
+ * Rvzware based in CAPAWARE 3D
  *
- * This file is part of CAPAWARE.
- *
- * CAPAWARE is free software; you can redistribute it and/or modify it under
+ * Rvzware is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
  *
- * CAPAWARE is distributed in the hope that it will be useful, but WITHOUT
+ * Rvzware is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
@@ -18,8 +15,9 @@
  * along with this application; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * The CAPAWARE development team
-*/
+ * The Rvzware development team
+ */
+
 #ifndef _SUNCONTROLLER_
 #define _SUNCONTROLLER_
 
