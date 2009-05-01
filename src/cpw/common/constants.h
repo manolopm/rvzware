4c9625188099676d4a6e9b6792575170fcc2a944
Manolopm <manolopm@gmail.com>
2009-05-01 12:24:23 +0100

    Fixed some strings conversions

--- src/cpw/common/constants.h
+++ src/cpw/common/constants.h
@@ -32,30 +32,30 @@ const double rad2deg = 180.0 / PI;
 
 static Ellipsoid ellipsoid[] = 
 {//  id, Ellipsoid name, Equatorial Radius, square of eccentricity	
-	Ellipsoid( -1, "Placeholder", 0, 0),//placeholder only, To allow array indices to match id numbers
-	Ellipsoid( 1, "Airy", 6377563, 0.00667054),
-	Ellipsoid( 2, "Australian National", 6378160, 0.006694542),
-	Ellipsoid( 3, "Bessel 1841", 6377397, 0.006674372),
-	Ellipsoid( 4, "Bessel 1841 (Nambia) ", 6377484, 0.006674372),
-	Ellipsoid( 5, "Clarke 1866", 6378206, 0.006768658),
-	Ellipsoid( 6, "Clarke 1880", 6378249, 0.006803511),
-	Ellipsoid( 7, "Everest", 6377276, 0.006637847),
-	Ellipsoid( 8, "Fischer 1960 (Mercury) ", 6378166, 0.006693422),
-	Ellipsoid( 9, "Fischer 1968", 6378150, 0.006693422),
-	Ellipsoid( 10, "GRS 1967", 6378160, 0.006694605),
-	Ellipsoid( 11, "GRS 1980", 6378137, 0.00669438),
-	Ellipsoid( 12, "Helmert 1906", 6378200, 0.006693422),
-	Ellipsoid( 13, "Hough", 6378270, 0.00672267),
-	Ellipsoid( 14, "International", 6378388, 0.00672267),
-	Ellipsoid( 15, "Krassovsky", 6378245, 0.006693422),
-	Ellipsoid( 16, "Modified Airy", 6377340, 0.00667054),
-	Ellipsoid( 17, "Modified Everest", 6377304, 0.006637847),
-	Ellipsoid( 18, "Modified Fischer 1960", 6378155, 0.006693422),
-	Ellipsoid( 19, "South American 1969", 6378160, 0.006694542),
-	Ellipsoid( 20, "WGS 60", 6378165, 0.006693422),
-	Ellipsoid( 21, "WGS 66", 6378145, 0.006694542),
-	Ellipsoid( 22, "WGS-72", 6378135, 0.006694318),
-	Ellipsoid( 23, "WGS-84", 6378137, 0.00669438)
+  Ellipsoid( -1, (char *)((std::string) "Placeholder").c_str(), 0, 0),//placeholder only, To allow array indices to match id numbers
+	Ellipsoid( 1, (char *)((std::string) "Airy").c_str(), 6377563, 0.00667054),
+	Ellipsoid( 2, (char *)((std::string) "Australian National").c_str(), 6378160, 0.006694542),
+	Ellipsoid( 3, (char *)((std::string) "Bessel 1841").c_str(), 6377397, 0.006674372),
+	Ellipsoid( 4, (char *)((std::string) "Bessel 1841 (Nambia) ").c_str(), 6377484, 0.006674372),
+	Ellipsoid( 5, (char *)((std::string) "Clarke 1866").c_str(), 6378206, 0.006768658),
+	Ellipsoid( 6, (char *)((std::string) "Clarke 1880").c_str(), 6378249, 0.006803511),
+	Ellipsoid( 7, (char *)((std::string) "Everest").c_str(), 6377276, 0.006637847),
+	Ellipsoid( 8, (char *)((std::string) "Fischer 1960 (Mercury) ").c_str(), 6378166, 0.006693422),
+	Ellipsoid( 9, (char *)((std::string) "Fischer 1968").c_str(), 6378150, 0.006693422),
+	Ellipsoid( 10, (char *)((std::string) "GRS 1967").c_str(), 6378160, 0.006694605),
+	Ellipsoid( 11, (char *)((std::string) "GRS 1980").c_str(), 6378137, 0.00669438),
+	Ellipsoid( 12, (char *)((std::string) "Helmert 1906").c_str(), 6378200, 0.006693422),
+	Ellipsoid( 13, (char *)((std::string) "Hough").c_str(), 6378270, 0.00672267),
+	Ellipsoid( 14, (char *)((std::string) "International").c_str(), 6378388, 0.00672267),
+	Ellipsoid( 15, (char *)((std::string) "Krassovsky").c_str(), 6378245, 0.006693422),
+	Ellipsoid( 16, (char *)((std::string) "Modified Airy").c_str(), 6377340, 0.00667054),
+	Ellipsoid( 17, (char *)((std::string) "Modified Everest").c_str(), 6377304, 0.006637847),
+	Ellipsoid( 18, (char *)((std::string) "Modified Fischer 1960").c_str(), 6378155, 0.006693422),
+	Ellipsoid( 19, (char *)((std::string) "South American 1969").c_str(), 6378160, 0.006694542),
+	Ellipsoid( 20, (char *)((std::string) "WGS 60").c_str(), 6378165, 0.006693422),
+	Ellipsoid( 21, (char *)((std::string) "WGS 66").c_str(), 6378145, 0.006694542),
+	Ellipsoid( 22, (char *)((std::string) "WGS-72").c_str(), 6378135, 0.006694318),
+	Ellipsoid( 23, (char *)((std::string) "WGS-84").c_str(), 6378137, 0.00669438)
 };
 
 #endif
